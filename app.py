cf74c746ecdfd1e861aa9eb30fcb259405805a8d
nmhk <football.nh0430@icloud.com>
2020-02-04 09:54:20 +0900

    ゲームのユーザー名を表現するUserNameクラスの作成
    - 文字数に関するルールを一箇所にまとめた
    - `screen_name()`で管理したので、要件が変わっても変更が楽

--- app.py
+++ app.py
@@ -1,2 +1,28 @@
-class Username:
-    def __
+'''
+
+
+'''
+
+
+class UserName:
+    def __init__(self, name):
+        if not (4 <= len(name) <= 20):
+            raise ValueError(f'{name}は文字数のルール違反やで！')
+
+        self.name = name
+
+    def screen_name(self):
+        return self.name.upper()
+
+
+# UserNameクラスのインスタンス化
+hibiki = UserName(name='hibiki')
+
+print(hibiki.screen_name())
+print(hibiki.name.upper())
+# print('hibiki')
+# print(type(hibiki))
+
+
+# sho = UserName('sho')
+# print(sho.name)
